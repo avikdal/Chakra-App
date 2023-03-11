3f60e5c76d260d4f69a587bfe04b75b2456fd625
amanda vikdal <vikdal.amanda@gmail.com>
2023-03-10 18:29:38 -0700

    created home page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from "react-router-dom";
 import Header from "./Header"
-import NavBar from "./NavBar"
 import Home from "./Home";
 import About from "./About";
 import Chakras from "./Chakras";
 
 function App() {
+  // const [chakraData, setChakraData] = useState([])
+
+  fetch('http://localhost:3000/chakraData')
+  .then(r => r.json())
+  .then(chakra => console.log(chakra))
   console.log("hi")
-  
+
   return (
     <div className="App">
       <Header />
-      <NavBar />
       <Switch>
         <Route path="/about">
           <About />
